0f6309632963b05d09711e1549fe3faf8bdac4bf
iscorpion <masteringzelda@gmail.com>
2017-06-10 17:44:06 -0300

    Inicial
    Estrutura das pastas pedida pelo Tiago

---
+++ GRUPO05_AM_PROJ/IMPLEMENTACAO/Grupo_05.m
@@ -0,0 +1,54 @@
+%% Universidade Federal de Sao Carlos - UFSCar, Sorocaba
+%
+%  Disciplina: Aprendizado de M�quina
+%  Prof. Tiago A. Almeida
+%
+%  Trabalho 01 - Censo 1994 - Quem Ganha Mais de 50 Mil?
+%
+%  Grupo 05:
+%  ----------
+%  Celso Martins De Araujo Filho
+%
+%  Vinnícius Ferreira da Silva
+%
+%  Renan Ferreira de Almeida
+%  ----------
+%
+%
+
+%% Inicializacao
+clear ; close all; clc
+
+%% ===================================================== %%
+%% Carregamento dos dados
+
+
+
+%% ===================================================== %%
+%% Preprocessamento dos dados
+
+
+%% ===================================================== %%
+%% Menu Principal:
+%% Opcoes:
+%%    1 - Teste de parametros
+%%    2 - Classificacao dos Dados
+
+
+%% ===================================================== %%
+%% Opcao 1 - Teste de Parametros
+%%    Submenu 1 - Escolha de algoritmo para testar
+%%        > knn (valores de K)
+%%        > regressao logistica (lambda)
+%%        > redes neurais
+%%        > SVM
+%% 
+
+%% ===================================================== %%
+%% Opcao 2 - Classificacao dos Dados
+%%    Submenu 2 - Escolha um algoritmo de classificacao
+%%        > knn (valores de K)
+%%        > regressao logistica (lambda)
+%%        > redes neurais
+%%        > SVM
+%%
---
+++ GRUPO05_AM_PROJ/IMPLEMENTACAO/holdout.m
@@ -0,0 +1,28 @@
+function [X_train, Y_train, X_test, Y_test] = holdout(X, Y, S)
+% HOLDOUT aplica uma divisao das amostras de X e Y em conjuntos
+% de teste e de treinamento. A escolha de amostra eh aleatoria,
+% e X_train e X_test sao subconjuntos disjuntos de X.
+
+% Parametros:
+%   X: conjunto de dados original
+%   Y: conjunto de atributos alvo 
+%   S: numero [0,1] indicando a divisao entre conjunto de treino 
+%   e conjunto de teste. Ex.: S = 0.8 = 80% treinamento, 20% teste.
+
+
+% Tamanho do conjunto de dados (linhas)
+num_points = size(X,1);
+% Define o ponto de divisao com base no parametro S
+split = round(num_points*S);
+% Cria uma sequencia aleatoria representando os indices de X
+seq = randperm(num_points);
+
+% Divisao de X e Y em conjunto de treinamento
+X_train = X(seq(1:split),:);
+Y_train = Y(seq(1:split));
+
+% Divisao de X e Y em conjunto de teste
+X_test = X(seq(split+1:end),:);
+Y_test = Y(seq(split+1:end),:);
+
+end
